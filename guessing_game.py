c8f909fb3661c8d9da95e185c3565bf7734c5247
lilukai1 <50470636+lilukai1@users.noreply.github.com>
2020-11-08 13:38:37 -0600

    file renaming

--- guessing_game.py
+++ guessing_game.py
@@ -1,33 +1,22 @@
 ## A simple 'guess the number' game! ##
 
 import random
-import pdb
 
-guesses = 0
-
-solved = False
-
-
-def start_game():
-    global solved
-    global answer
-    global guesses
-
-    solved = False
-    answer = random.randrange(1,10)
-    guesses = 0
-    game_on()
-
+## maybe add extra functionality? Like displaying what the old scores were, or how many times the game has been played ##
+scores = [100000,]
 
 def game_on():
-    global solved
-    global guesses
+    solved = False
+    answer = random.randrange(1,11)
+    guesses = 1
     while solved == False:
         try:
             guess = int(input("Guess a number between 1 and 10.\n"))
+            if guess > 10 or guess < 1:
+                raise 
         except:
             print("That's not a valid option!\n")
-
+            continue
         if guess > answer:
             print("Your guess was too high!")
             guesses += 1
@@ -36,19 +25,29 @@ def game_on():
             guesses += 1
         elif guess == answer:
             solved = True
-            game_winner()
+            game_winner(guesses)
 
 
-def game_winner():
-    print(f"You won the game!! It took you {guesses} guesses.")
-    play_again = None
+def game_winner(guesses):
+
+    high_score = min(scores)
+    play_count = len(scores)
+    if high_score > guesses:
+        print(f"You won game {play_count} AND beat the old high score!!  New high score is {guesses} guesses.")
+    elif high_score == guesses:
+        print(f"You won game {play_count} AND tied the high score!! High score is {guesses} guesses.")
+    else:
+        print(f"You won game {play_count}!! It took you {guesses} guesses.  The current high score is {high_score} guesses.")
+    
+    play_again = ""
+    scores.append(guesses)
+
     while play_again.lower() not in ("y", "n"):
-        try:
             play_again = input("Would you like to play again? Y/N \n")
-            if play_again.lower() != "y" or play_again.lower() != "n":
-                print("Sorry, I didn't understand.  Please press Y for yes and N for no.")
             if play_again.lower() == "y":
-                start_game()
-            else:
+                game_on()
+            elif play_again.lower() == "n":
                 print("Thank you for playing!")
-# start_game()
+
+
+game_on()
